9fb11262d7a0bb36b100d55bf13656a7c15ea0cd
KingKayDee <kwekudeheer@ymail.com>
2020-10-27 00:48:52 +0000

    Testing out more example strings

--- ToString/Source Code/Stringify/StringifyFloatingPoint.hpp
+++ ToString/Source Code/Stringify/StringifyFloatingPoint.hpp
@@ -6,7 +6,6 @@
 #include <typeinfo>
 
 #include "../StringConverter.hpp"
-#include "../kStringManipulation.hpp"
 
 namespace klib::kFormat::stringify
 {
@@ -14,7 +13,7 @@ namespace klib::kFormat::stringify
 		, typename = std::enable_if_t<std::is_floating_point_v<T>
 		|| type_trait::Is_CharType_V<CharType>>
 		>
-		kString::StringWriter<CharType> StringFloatingPoint(const T val, size_t precision, std::chars_format fmt = std::chars_format::general)
+		kString::StringWriter<CharType> StringFloatingPoint(const T val, size_t precision, std::chars_format fmt = std::chars_format::fixed)
 	{
 		if (precision == nPrecision)
 			precision = 6;
@@ -23,23 +22,27 @@ namespace klib::kFormat::stringify
 		char buff[maxsize]{};
 		char* const end = std::end(buff);
 		
-#if _HAS_COMPLETE_CHARCONV
+#if defined(_HAS_COMPLETE_CHARCONV) && (_HAS_COMPLETE_CHARCONV == FALSE)
+		const std::string format = "%." + std::to_string(precision) + "f";
+		const auto length = _snprintf(buff, maxsize, format.data(), val);
+		const auto conv = kString::Convert<CharType>(buff);
+		const kString::StringWriter<CharType> str(conv, conv + length);
+#else
 		const std::to_chars_result res = std::to_chars(buff, end, val, fmt, static_cast<int>(precision));
 
 		if (res.ec != std::errc{})
 		{
 			const std::string type = typeid(T).name();
 			const std::string n = std::to_string(val);
-			throw std::runtime_error("Bad kFormat floating point cast: " + type + " - " + n);
+			const std::string ptr = res.ptr;
+			auto err = "Bad " __FUNCSIG__ " floating point cast: " + type + " - " + n;
+			err += "\n";
+			err += "Error - " + ptr;
+			throw std::runtime_error(err);
 		}
 
 		kString::StringWriter<CharType> str(kString::Convert<CharType>(buff));
-		HandleDecimalPrecision(str, precision, CharType('0'));
-#else
-		const std::string format = "%." + std::to_string(precision) + "f";
-		const auto length = _snprintf(buff, maxsize, format.data(), val);
-		const auto conv = kString::Convert<CharType>(buff);
-		const kString::StringWriter<CharType> str(conv, conv + length);
+		//HandleDecimalPrecision(str, precision, CharType('0'));
 #endif 
 
 		return str;
