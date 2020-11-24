02e90d013b57f600ff1c5a6c92f569f5becd72e5
Karel de Heer <karel.deheer@inseinc.com>
2020-11-24 18:58:22 +0000

    Set the locale before starting test

--- TestGround/Source Code/main.cpp
+++ TestGround/Source Code/main.cpp
@@ -1,9 +1,13 @@
 #include "Testing/TesterManager.hpp"
+#include <Utility/String/Tricks/kStringLocale.hpp>
+
 
 #include <iostream>
 
 int main()
 {
+	klib::kString::secret::impl::SetLocale("");
+	
 	auto& testMan = kTest::TesterManager::Get();
 	testMan.Initialize();
 	// testMan.InitializeMaths();
