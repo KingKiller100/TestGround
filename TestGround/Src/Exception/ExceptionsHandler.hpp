2566e683cb7c5ef8f1be18bb3ac747249f5af1a6
King Kay Dee <kwekudeheer@ymail.com>
2021-12-06 23:46:18 +0000

    Added exception handler for test main in case of early exceptions

---
+++ TestGround/Src/Exception/ExceptionsHandler.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <string>
+#include <stdexcept>
+
+namespace debug
+{
+	std::string UnwrapNestedExceptions( std::exception_ptr ePtr = std::current_exception() );
+}
--- TestGround/Src/TestMain.cpp
+++ TestGround/Src/TestMain.cpp
@@ -1,24 +1,49 @@
-#include <kLib.hpp>
+#include <kTest.hpp>
 #include <iostream>
+
+#include "Exception/ExceptionsHandler.hpp"
+
+void kLibTest();
+
+void FlushOutputs();
 
 int main()
 {
-	klib::kLocale::SetDefaultLocale();
-	klib::kCalendar::UsePlatformCalendarInfoSource();
-
-	auto testMan = kTest::TesterManager{};
-	testMan.Initialize(true);
-	kTest::InitializeMathsTests(testMan);
-	kTest::InitializeUtilityTests(testMan, false);
-	kTest::InitializeTemplateTests(testMan);
-	testMan.RunAll(std::thread::hardware_concurrency());
-	testMan.RunPerformanceTests();
-	testMan.Shutdown();
+	try
+	{
+		kLibTest();
+	}
+	catch ( ... )
+	{
+		std::cerr << "Exception occurred:\n";
+		std::cerr << debug::UnwrapNestedExceptions();
+	}
 
 	std::cout << "\nPress 'ENTER' to exit...";
-	std::cout.flush();
-	std::cerr.flush();
 	std::cin.get();
 
+	FlushOutputs();
+
 	return EXIT_SUCCESS;
+}
+
+void kLibTest()
+{
+		klib::kLocale::SetDefaultLocale();
+		klib::kCalendar::UsePlatformCalendarInfoSource();
+	kTest::TesterManager testMan;
+	testMan.Initialize( true );
+	kTest::InitializeMathsTests( testMan );
+	kTest::InitializeUtilityTests( testMan, false );
+	kTest::InitializeTemplateTests( testMan );
+	testMan.RunAll( std::thread::hardware_concurrency() );
+	testMan.RunPerformanceTests();
+	testMan.Shutdown();
+}
+
+void FlushOutputs()
+{
+	std::cout.flush();
+	std::cerr.flush();
+	std::clog.flush();
 }
